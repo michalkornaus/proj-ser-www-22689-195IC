a3f81c5259f59bea89e58dea292c9fe6ae430b96
michalkornaus <michal990909@wp.pl>
2021-11-01 01:16:17 +0100

    Rozbudowa strony

--- Lab1/js/scripts.js
+++ Lab1/js/scripts.js
@@ -5,3 +5,14 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+function setDate(){
+	document.getElementById("data").innerHTML = "Data ostatniej modyfikacji: " + document.lastModified;
+}
+
+function sendLetter(){
+	document.getElementById("letter").style.display = "none"; 
+	document.getElementById("nagl").innerHTML = "Wysłano list!"; 
+	setTimeout(function(){
+		alert("Wysłanie listu powiodło się");
+	}, 1000);
+}
