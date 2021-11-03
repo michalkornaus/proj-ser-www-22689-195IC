06399b22f72683390f1fd96a800acf065e6c9d68
michalkornaus <michal990909@wp.pl>
2021-11-03 21:21:38 +0100

    Obrazki + dokonczenie skryptow

--- js/textbook.js
+++ js/textbook.js
@@ -4,6 +4,11 @@ function replaceElement(){
 	var _p = document.createElement("p");
 	_p.className = "textCenter";
 	_p.style.font = "bold";
-	_p.innerHTML = "ALGORYTMY ALGORYTMY ALGORYTMY ALGORYTMY";
+	_p.innerHTML = "ALGORYTMY";
+	_p.id = "newP";
 	div.replaceChild(_p, oldP);
+}
+function addText(){
+	var _p = document.getElementById("newP");
+	_p.insertAdjacentHTML("afterbegin", "<span style='color:red'>ALGORYTMY </span>");
 }
